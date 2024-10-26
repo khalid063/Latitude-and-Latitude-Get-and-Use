fd4482fdc02cb5b28bb1e4d5c57cb33a5ba67eee
DeltaKhalid <khalid.delta063@gmail.com>
2024-10-26 13:01:31 +0600

    Lat and long get and show work has been done

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,7 +10,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       debugShowCheckedModeBanner: false,
       title: "Latitude and Longitude get",
       home: HomeScreen(),
@@ -25,46 +26,113 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+
+  ///================================================ All variables ====================================================///
+  String latitudeOfUser = '';
+  String longitudeOfUser = '';
+
+
+  ///================================================ Function for get "Let Long" =======================================///
+  Future<Position> _determinePosition() async {
+    bool serviceEnabled;
+    LocationPermission permission;
+
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      return Future.error('Location services are disabled.');
+    }
+
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        return Future.error('Location permissions are denied');
+      }
+    }
+
+    if (permission == LocationPermission.deniedForever) {
+      return Future.error(
+          'Location permissions are permanently denied, we cannot request permissions.');
+    }
+
+    return await Geolocator.getCurrentPosition();
+  }
+  // get let long
+  Future<void> _getUserLocation() async {
+    Position position = await _determinePosition();
+    double latitude = position.latitude;
+    double longitude = position.longitude;
+
+    latitudeOfUser = latitude.toString();
+    longitudeOfUser = longitude.toString();
+
+    setState(() {
+
+    });
+
+    // Do something with the latitude and longitude, like displaying them or sending them to a server
+    print("Latitude: $latitude, Longitude: $longitude");
+  }
+
+
+
+
+
+
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Latitude and Longitude get and Show'),
+        title: Text('Lat and Long get and Show'),
+        backgroundColor: Colors.cyan,
+        centerTitle: true,
       ),
       body: Center(
         child: Column(
           children: [
 
-            SizedBox(height: 50,),
+            const SizedBox(height: 50,),
             Text(
-              //'Latitude: $latitudeOfUser',
-              'Latitude: ',
+              'Latitude: $latitudeOfUser',
+              //'Latitude: ',
               style: const TextStyle(
                 fontSize: 30,
               ),
             ),
-            SizedBox(height: 50,),
+            const SizedBox(height: 50,),
             Text(
-              //'Latitude: $longitudeOfUser',
-              'Latitude: ',
+              'Latitude: $longitudeOfUser',
+              //'Latitude: ',
               style: const TextStyle(
                 fontSize: 30,
               ),
             ),
-            Row(
-              children: [
-                SizedBox(height: 50,),
-                ElevatedButton(
-                  //onPressed: _getUserLocation,
-                  onPressed: (){},
-                  child: const Text('Get Location'),
-                ),
-                SizedBox(height: 50,),
-                ElevatedButton(
-                  onPressed: (){},
-                  child: const Text('Reset Location'),
-                ),
-              ],
+            const SizedBox(height: 50,),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Row(
+                children: [
+                  const SizedBox(height: 50,),
+                  ElevatedButton(
+                    onPressed: _getUserLocation,
+                    //onPressed: (){},
+                    child: const Text('Get Location'),
+                  ),
+                  const Spacer(),
+                  ElevatedButton(
+                    onPressed: (){
+                      latitudeOfUser = "";
+                      longitudeOfUser = "";
+                      setState(() {
+
+                      });
+                    },
+                    child: const Text('Reset Location'),
+                  ),
+                ],
+              ),
             )
           ],
         ),
